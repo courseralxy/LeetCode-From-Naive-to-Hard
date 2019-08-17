b852ff0ed8c7127489b92a608e28f752a410b06e
courseralxy <liuxy@smail.nju.edu.cn>
2019-08-17 17:37:56 -0400

    Create 711.py
    711 with Python 3

---
+++ 711.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        count = 0
+        for i in J:
+            count += S.count(i)
+            S.replace(i,'')
+        return count
